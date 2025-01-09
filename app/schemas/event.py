b12b7d748b3a2170e758a053767deb691a506110
Aaron Fort Garcia <aaronfortg@gmail.com>
2025-01-09 01:09:40 +0100

    - Ara es pot crear un event gràcies a treure la time zone parsejada (BBDD no accepta)

--- app/schemas/event.py
+++ app/schemas/event.py
@@ -1,8 +1,17 @@
-from pydantic import BaseModel
+from datetime import datetime
+from pydantic import BaseModel, validator
 from typing import Optional
 
 class EventCreate(BaseModel):
     name: str
     price: float
     image_url: Optional[str] = None
-    event_date: str
+    event_date: datetime
+
+    @validator('event_date', pre=True)
+    def strip_timezone(cls, value):
+        if isinstance(value, str):
+            value = datetime.fromisoformat(value.replace('Z', '+00:00'))
+        if value.tzinfo is not None:
+            value = value.astimezone(tz=None).replace(tzinfo=None)
+        return value
